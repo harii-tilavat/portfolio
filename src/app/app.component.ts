64f1b7fa33143623676526fb46bccf5041be5b67
harit <harit123@gmail.com>
2023-06-22 19:00:13 +0530

    make some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+ @ViewChild('sidebar') sidebar!:ElementRef;
+  public title = 'portfolio';
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+  onSidebarChange(event: any) {
+    console.log("Value of Event: ", event);
+    console.log("Sidebar: ",this.sidebar);
+    this.sidebar.nativeElement.classList.toggle('show');
+  }
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent {
-
+export class HeroComponent implements OnInit {
+  @Output() sidebarEvent = new EventEmitter<boolean>();
+  public isDisplay:boolean=false;
+  constructor() { }
+  ngOnInit(): void {
+  }
+  onToggle():void{
+    this.isDisplay=!this.isDisplay;
+    this.sidebarEvent.emit(this.isDisplay);
+  }
 }
